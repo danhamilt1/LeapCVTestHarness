75aa94cf69f76dd8787b1105fb8e59be5906d3db
danhamilt1 <daniel2.hamilton@live.uwe.ac.uk>
2015-03-13 12:17:01 +0000

    following leapcv

--- src/resources/MainController.java
+++ src/resources/MainController.java
@@ -6,8 +6,13 @@ import java.io.File;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
+import org.opencv.highgui.Highgui;
 
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.TimelineBuilder;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
@@ -15,13 +20,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
+import javafx.util.Duration;
 
 import com.smashthestack.*;
 
 public class MainController {
 
-	 @FXML
+	 	@FXML
 	    private Canvas image;
+	 	@FXML
+	    private Canvas image2;
 
 	    @FXML
 	    private TextField lambda;
@@ -56,11 +64,18 @@ public class MainController {
 	private LeapCVController leapController;
 	private LeapCVStereoUtils leapStereo;
 
+	@SuppressWarnings("deprecation")
 	public void initialize() {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		this.leapController = new LeapCVController();
+		try {
+			this.leapController = new LeapCVController();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		this.leapStereo = new LeapCVStereoUtils();
-
+		
 		Platform.runLater(()->{
 			this.fi.setText(String.valueOf(this.leapStereo.getFi()));
 			this.lambda.setText(String.valueOf(this.leapStereo.getLambda()));
@@ -74,11 +89,23 @@ public class MainController {
 			
 		});
 
+		AnimationTimer timer = new AnimationTimer() {
+			
+			@Override
+			public void handle(long now) {
+				// TODO Auto-generated method stub
+				onSnapTaken(new ActionEvent());
+			}
+		};
+		
+		timer.start();
+		
+		
 	}
 
 	@FXML
 	void onSnapTaken(ActionEvent event) {
-		Rect roi = new Rect(120,140,400,200);
+		Rect roi = new Rect(160,160,320,320);
 		
 		this.leapStereo.setFi(Double.valueOf(this.fi.getText()));
 		this.leapStereo.setLambda(Double.valueOf(this.lambda.getText()));
@@ -90,7 +117,7 @@ public class MainController {
 		this.leapStereo.setPyrScale(Double.valueOf(this.pyrScale.getText()));
 		this.leapStereo.setCycle(Double.valueOf(this.cycle.getText()));
 		
-//		//this.fi.setText(String.valueOf(this.leapStereo.getFi()));
+		//this.fi.setText(String.valueOf(this.leapStereo.getFi()));
 //		this.lambda.setText(String.valueOf(this.leapStereo.getLambda()));
 //		this.levels.setText(String.valueOf(this.leapStereo.getLevels()));
 //		this.maxDisp.setText(String.valueOf(this.leapStereo.getMaxDisp()));
@@ -98,15 +125,34 @@ public class MainController {
 //		this.polyN.setText(String.valueOf(this.leapStereo.getPolyN()));
 //		this.polySigma.setText(String.valueOf(this.leapStereo.getPolySigma()));
 //		this.pyrScale.setText(String.valueOf(this.leapStereo.getPyrScale()));
-		
-		this.leapController.moveToNextValidFrame();
-		Mat left = this.leapController.getLeftImageUndistorted();
-		Mat right = this.leapController.getRightImageUndistorted();
-		Mat image = this.leapStereo.getDisparityMap(left.submat(roi), right.submat(roi));
-		Image image2 = LeapCVImageUtils.matToWritableImage(image);
-		this.image.getGraphicsContext2D().drawImage(image2, 0, 0);
-		
-		this.leapStereo.savePointCloud(this.leapStereo.getPointCloud(image), new File("Pointcloud.obj"));
+		try{
+			this.leapController.nextValidFrame();
+			
+			Mat left = this.leapController.getLeftImageUndistorted();//Undistorted();
+			
+			Mat right = Highgui.imread("match.png");//= this.leapController.getRightImageUndistorted();//Undistorted();
+			//LeapCVObjectDetector.sift(left);
+			
+			//LeapCVObjectDetector.sift(right);
+			//Mat image = this.leapStereo.getDisparityMap(left.submat(roi), right.submat(roi));
+			
+			
+			//Highgui.imwrite("outl.jpg", this.leapController.getLeftImageUndistorted().submat(roi));
+			//Highgui.imwrite("outr.jpg", this.leapController.getRightImageUndistorted().submat(roi));
+			
+			//this.leapStereo.savePointCloud(this.leapStereo.getPointCloud(image), new File("Pointcloud.obj"));
+			
+			//Highgui.imwrite("match.png", this.leapController.getRightImageUndistorted());
+			//System.exit(0);
+			
+			Image image2 = LeapCVImageUtils.matToWritableImage(LeapCVObjectDetector.match(left.submat(roi), right));//.submat(roi)));
+			this.image.getGraphicsContext2D().drawImage(image2, 0, 0);
+//			image2 = LeapCVImageUtils.matToWritableImage(right.submat(roi));
+//			this.image2.getGraphicsContext2D().drawImage(image2, 0, 0);
+		} catch (Exception e){
+			e.printStackTrace();
+			System.out.println("The leap motion is not connected");
+		}
 	}
 
 }
